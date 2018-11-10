2a80280ff520294ba19b3e10c35b1bb488a57ba3
Jasmine <jasmine.vo@utdallas.edu>
2018-11-10 17:11:46 -0600

    Lab 2 required material

---
+++ CreationViewController.swift
@@ -0,0 +1,58 @@
+//
+//  CreationViewController.swift
+//  FlashcardApp
+//
+//  Created by Jasmine Vo on 10/31/18.
+//  Copyright © 2018 Jasmine Vo. All rights reserved.
+//
+
+import UIKit
+
+class CreationViewController: UIViewController {
+    
+    var flashcardsController: ViewController!
+    
+    @IBOutlet weak var questionTextField: UITextField!
+    @IBOutlet weak var answerTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        }
+        // Do any additional setup after loading the view.
+    @IBAction func didTapOnCancel(_ sender: Any) {
+        dismiss(animated: true)
+    }
+    
+    @IBAction func didTapOnDone(_ sender: Any) {
+        
+        //get text in the question text field
+        let questionText = questionTextField.text
+        
+        //get text in the answer text field
+        let answerText = answerTextField.text
+        
+        //call the function to update the flashcard
+        flashcardsController.updateFlashcard(question: questionText!, answer: answerText!)
+        
+        //dismiss
+        dismiss(animated: true)
+    }
+    
+    
+}
+   
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+
--- FlashcardApp/ViewController.swift
+++ FlashcardApp/ViewController.swift
@@ -18,9 +18,38 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    @IBAction func didTapOnFlashcard(_ sender: Any) {
-        frontLabel.isHidden = true
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // let the segue know the destination is the Navigation Controller
+        let navigationController = segue.destination as! UINavigationController
+        
+        // we know the navigation controller only contains a creation view controller
+        let creationController = navigationController.topViewController as! CreationViewController
+        
+        // we set flashcardController property to self
+        creationController.flashcardsController = self
     }
+    
+    @IBAction func didTapOnFlashcard(_ sender: Any) {
+        if frontLabel.isHidden == true {
+            frontLabel.isHidden = false
+        }
+        else {
+        frontLabel.isHidden = true
+        }
+    }
+    
+    func updateFlashcard(question: String, answer: String) {
+        
+        frontLabel.text = question
+        backLabel.text = answer
+    }
+    
+   // @IBAction func didTapOnDone(_ sender: Any) {
+   //   dismiss(animated: true)
+   //  }
+    
+
+    
     
 }
 
